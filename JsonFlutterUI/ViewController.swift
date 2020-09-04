80cc6165d18143b01b8c2ec811317e96f7b73909
KoingDev <sea.saingkoing@gmail.com>
2020-09-05 01:28:05 +0900

    Initial Commit

---
+++ JsonFlutterUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonFlutterUI
+//
+//  Created by KoingDev on 9/5/20.
+//  Copyright © 2020 koing.dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
